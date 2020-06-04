96580e44d043a6d5950215e71e8affd004649b59
tariktabakovic <tariktab97@gmail.com>
2020-06-04 12:24:22 -0400

    gives main content styling

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ function App() {
   return (
     <div className="App">
       <Toolbar/>
+      <main style={{marginTop: '64px'}}>
+        <p>This is the page content</p></main>
     </div>
   );
 }
