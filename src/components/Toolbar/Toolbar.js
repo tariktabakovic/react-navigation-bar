574afee56c0025b1fca15b6a328c62d3e7e212a3
tariktabakovic <tariktab97@gmail.com>
2020-06-04 12:16:10 -0400

    creates toolbar component with nav tag

---
+++ src/components/Toolbar/Toolbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const toolbar = props => (
+    <header>
+        <nav>
+            <div></div>
+            <div><a href="/">The Logo</a></div>
+            <div>
+                <ul>
+                    <li><a href="/"></a>Products</li>
+                    <li><a href="/"></a>Users</li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+);
+
+export default toolbar;
