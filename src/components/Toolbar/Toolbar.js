59c4b59d9610f58c06ee37b03dfd0eec9734ab0d
tariktabakovic <tariktab97@gmail.com>
2020-06-04 12:20:07 -0400

    adds className to elements

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './Toolbar.css'
 
 const toolbar = props => (
-    <header>
-        <nav>
+    <header className="toolbar">
+        <nav className="toolbar_navigation">
             <div></div>
-            <div><a href="/">The Logo</a></div>
-            <div>
+            <div className="toolbar_logo"><a href="/">The Logo</a></div>
+            <div className="toolbar_navigation-items">
                 <ul>
                     <li><a href="/"></a>Products</li>
                     <li><a href="/"></a>Users</li>
